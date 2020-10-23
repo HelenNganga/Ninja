ca34c04652df407a635e4d052a82eba086bcae49
HelenNganga <helennganga53@gmail.com>
2020-10-23 12:13:01 +0200

    tui works in browser

--- ninja/app/controllers/NinjaController.scala
+++ ninja/app/controllers/NinjaController.scala
@@ -3,7 +3,9 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import de.htwg.se.ninja.NinjaGame
-import scala.swing.Publisher
+import de.htwg.se.ninja.controller.component.State
+import de.htwg.se.ninja.model.component.component.component.component.Direction
+
 
 
 /**
@@ -16,7 +18,41 @@ class NinjaController @Inject()(cc: ControllerComponents) extends AbstractContro
 
   def ninjaAsText = NinjaGame.tui.stateToString()
 
+  def newGame() = Action{
+    gameController.newGame()
+    Ok(NinjaGame.tui.stateToString())
+  }
 
+
+  def player(player: String)= Action{
+    gameController.setName(player)
+    Ok(NinjaGame.tui.stateToString())
+  }
+
+  def setFlag(row: Int, col: Int)= Action{
+    gameController.setFlag(row,col)
+    Ok(NinjaGame.tui.stateToString())
+  }
+
+  def walk(row: Int, col: Int, d: String)= Action{
+
+    val dir = StringToDirection(d)
+    print(dir)
+    gameController.walk(row,col,dir)
+    Ok(NinjaGame.tui.stateToString())
+  }
+
+  def changeTurn()= Action{
+    gameController.changeTurns()
+    Ok(NinjaGame.tui.stateToString())
+  }
+
+  def StringToDirection(d: String): Direction.direction = d match {
+    case "r" => Direction.right
+    case "l" => Direction.left
+    case "u" => Direction.up
+    case "d" => Direction.down
+    case _  => Direction.up }
 
   def about = Action {
     Ok(views.html.index())
--- ninja/build.sbt
+++ ninja/build.sbt
@@ -1,3 +1,4 @@
+
 name := """play-scala-seed"""
 organization := "com.example"
 
@@ -5,13 +6,16 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.13.3"
+scalaVersion := "2.12.7"
 
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "org.scala-lang.modules" % "scala-swing_2.12" % "2.0.3"
 
-// Adds additional packages into Twirl
-//TwirlKeys.templateImports += "com.example.controllers._"
+libraryDependencies += "com.google.inject" % "guice" % "4.1.0"
+libraryDependencies += "net.codingwell" %% "scala-guice" % "4.1.0"
+
+
 
 // Adds additional packages into conf/routes
 // play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
